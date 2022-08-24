47f3345029115f15cb7dd49412ab9d0e301f7c20
jannhakk <Janne.Hakkarainen@metropolia.fi>
2022-08-24 11:47:00 +0300

    completed task2 c

---
+++ components/hooks/ApiHooks.js
@@ -0,0 +1,40 @@
+import {useEffect, useState} from 'react';
+
+//const url = 'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+const apiUrl = 'https://media.mw.metropolia.fi/wbma/';
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(apiUrl + 'media?limit=5');
+      const json = await response.json();
+      console.log(json);
+      const allMediaData = json.map(async (mediaItem) => {
+        const response = await fetch(apiUrl + 'media/' + mediaItem.file_id);
+        return await response.json();
+      });
+
+      setMediaArray(await Promise.all(allMediaData));
+    } catch (error) {
+      console.log('media fetch failed', error);
+    }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+};
+/*
+  let mediaArray = [];
+   const loadMedia = async () => {
+     const response = await fetch(url);
+     const json = await response.json();
+     console.log(json);
+   };
+  }
+  */
+
+const useUser = () => {};
+
+export {useMedia, useUser};
